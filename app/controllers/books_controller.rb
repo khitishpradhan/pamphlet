162904cceb2b84192968d4dd83dd95255d6779a4
khitish <kitzx1999@gmail.com>
2021-06-23 22:39:51 +0530

    exception handling

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,11 +4,10 @@ class BooksController < ApplicationController
 
   def search
     @query = params[:query]
-    @url = "http://openlibrary.org/search.json?q=#{@query}&limit=10"
+    @url = "http://openlibrary.org/search.json?q=#{@query}&limit=12"
     @uri = URI(@url)
     @response = Net::HTTP.get(@uri)
-    @output = JSON.parse(@response)
-    @books = @output["docs"]
+    @books = JSON.parse(@response)["docs"]
   end
 
   def show
@@ -16,22 +15,24 @@ class BooksController < ApplicationController
 
     @review = Review.new
 
-    @isbn = params[:isbn]
-    @author_url = "https://openlibrary.org/api/books?bibkeys=ISBN:#{@isbn}&jscmd=details&format=json"
     @url = "https://openlibrary.org/works/#{@id}.json"
     @uri = URI(@url)
-    @author_uri = URI(@author_url)
-    @author_response = Net::HTTP.get(@author_uri)
     @response = Net::HTTP.get(@uri)
     @book = JSON.parse(@response)
-    @author = JSON.parse(@author_response)["ISBN:#{@isbn}"]["details"]["authors"][0]["name"]
-    @preview = JSON.parse(@author_response)["ISBN:#{@isbn}"]["preview_url"]
 
-    @reviews = Review.all
+    if (params[:isbn])
+      @isbn = params[:isbn]
+      @author_url = "https://openlibrary.org/api/books?bibkeys=ISBN:#{@isbn}&jscmd=details&format=json"
+      @author_uri = URI(@author_url)
+      @author_response = Net::HTTP.get(@author_uri)
+      @author = JSON.parse(@author_response)["ISBN:#{@isbn}"]["details"]["authors"][0]["name"]
+      @preview = JSON.parse(@author_response)["ISBN:#{@isbn}"]["preview_url"]
+      @suggestion_url = "http://openlibrary.org/search.json?author=#{@author}&limit=3"
+      @suggestion_uri = URI(@suggestion_url)
+      @suggestion_response = Net::HTTP.get(@suggestion_uri)
+      @suggestions = JSON.parse(@suggestion_response)["docs"]
+    end
 
-    @suggestion_url = "http://openlibrary.org/search.json?author=#{@author}&limit=2"
-    @suggestion_uri = URI(@suggestion_url)
-    @suggestion_response = Net::HTTP.get(@suggestion_uri)
-    @suggestions = JSON.parse(@suggestion_response)["docs"]
+    @reviews = Review.where(book_id: "#{@id}")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get "/books", to: "books#search"
 
   get "/book/works/:id/:isbn", to: "books#show"
+  get "/book/works/:id", to: "books#show"
 
   post "/reviews", to: "reviews#new"
 
