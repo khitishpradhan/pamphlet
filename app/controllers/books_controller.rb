2fe5be02bb42e513e1c11be6467681ef87180878
khitish <kitzx1999@gmail.com>
2021-06-24 22:51:51 +0530

    styling and exceptions

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,7 +7,8 @@ class BooksController < ApplicationController
     @url = "http://openlibrary.org/search.json?q=#{@query}&limit=12"
     @uri = URI(@url)
     @response = Net::HTTP.get(@uri)
-    @books = JSON.parse(@response)["docs"]
+    @output = JSON.parse(@response)
+    @books = @output["docs"]
   end
 
   def show
@@ -20,19 +21,24 @@ class BooksController < ApplicationController
     @response = Net::HTTP.get(@uri)
     @book = JSON.parse(@response)
 
-    if (params[:isbn])
-      @isbn = params[:isbn]
-      @author_url = "https://openlibrary.org/api/books?bibkeys=ISBN:#{@isbn}&jscmd=details&format=json"
-      @author_uri = URI(@author_url)
-      @author_response = Net::HTTP.get(@author_uri)
-      @author = JSON.parse(@author_response)["ISBN:#{@isbn}"]["details"]["authors"][0]["name"]
-      @preview = JSON.parse(@author_response)["ISBN:#{@isbn}"]["preview_url"]
+    @isbn = params[:isbn]
+    @author_url = "https://openlibrary.org/api/books?bibkeys=ISBN:#{@isbn}&jscmd=details&format=json"
+    @author_uri = URI(@author_url)
+    @author_response = Net::HTTP.get(@author_uri)
+
+    @author_json = JSON.parse(@author_response)["ISBN:#{@isbn}"]["details"]["authors"]
+
+    if (@author_json == nil)
+      @suggestions = []
+      @author = []
+    else
+      @author = @author_json[0]["name"]
       @suggestion_url = "http://openlibrary.org/search.json?author=#{@author}&limit=3"
       @suggestion_uri = URI(@suggestion_url)
       @suggestion_response = Net::HTTP.get(@suggestion_uri)
       @suggestions = JSON.parse(@suggestion_response)["docs"]
     end
-
+    @preview = JSON.parse(@author_response)["ISBN:#{@isbn}"]["preview_url"]
     @reviews = Review.where(book_id: "#{@id}")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   get "/books", to: "books#search"
 
   get "/book/works/:id/:isbn", to: "books#show"
-  get "/book/works/:id", to: "books#show"
 
   post "/reviews", to: "reviews#new"
 
