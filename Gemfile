48fa728d970d6f96d689fa2eba336c8a4fe879cb
khitish <kitzx1999@gmail.com>
2021-07-22 15:53:14 +0530

    UI fixes

--- Gemfile
+++ Gemfile
@@ -58,3 +58,7 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem "tzinfo-data", platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem 'solargraph', group: :development
+
+gem 'rubocop', group: :development, require: false
--- app/javascript/components/BookDetails.js
+++ app/javascript/components/BookDetails.js
@@ -9,7 +9,7 @@ const BookDetails = ({book, preview, author}) => {
 
     return (
     <div className="row">
-		<div className="col-sm-3 d-flex flex-column justify-content-center align-items-center p-2">
+		<div className="col-sm-3 d-flex flex-column justify-content-center text-center py-2">
            {book.covers? (
 			<img src={`http://covers.openlibrary.org/b/id/${book.covers[0]}-L.jpg`} className="book_image" />
 		   ) : (
@@ -17,13 +17,15 @@ const BookDetails = ({book, preview, author}) => {
 		   )}
 
 		   {preview? (
-			   <a href={`#${preview}`} className="btn btn-secondary preview_btn">Preview</a>
+			   <div>
+			   		<a href={`#${preview}`} className="btn btn-secondary preview_btn">Preview</a>
+			   </div>
 		   ) : (
 			<button className="btn btn-secondary preview_btn disabled" href="#" disabled={true}>Resource not available</button>
 		   )}
 
 		</div>
-		<div className="col-sm-9 p-2">
+		<div className="col-sm-9 py-2">
 			<h1>{book.title}</h1>
 			{author? (
 				<h6>by {author}</h6>
